80efbeeeab12aab7003c8d51052219d8d91fc437
SamCub <samule10cubano@gmail.com>
2025-01-13 23:57:41 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+
+    site: 'https://SamuelCubano.github.io',
+    base: 'Mi-Portafolio',
+
+
   devToolbar: {
       enabled: false
     },
 
   integrations: [icon()]
+
+ 
 });
